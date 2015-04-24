2004fbd3c63e6f02bf4569a3b609e8efb1a4a8f9
Vashistha Aggarwal <vishu.iitd@gmail.com>
2015-04-24 22:38:19 +0530

    home page v1

--- index.php
+++ index.php
@@ -157,93 +157,120 @@
 
 
 <!-- Start content -->
-<div id="content">
-	<div class="container">
+<div id="content" style="padding:0">
+	<div  style="background:url(images/patterns/1.png);" class="slider-main">
+		<div class="container slider">
 		
-		<!-- Start Services Icons -->
-		<div class="row">
-			
-			<!-- Start Service Icon 1 -->
-			<div class="col-md-9">
-				<!-- Start Home Page Slider -->
-			        <section id="home">
-			            <!-- Carousel -->
-			            <div id="main-slide" class="carousel slide" data-ride="carousel">
+			<!-- Start Services Icons -->
+			<div class="row">
+				
+				<!-- Start Service Icon 1 -->
+				<div class="col-md-9 slider">
+					<!-- Start Home Page Slider -->
+				        <section id="home">
+				            <!-- Carousel -->
+				            <div id="main-slide" class="carousel slide" data-ride="carousel">
 
-			                <!-- Indicators -->
-			                <ol class="carousel-indicators">
-			                    <li data-target="#main-slide" data-slide-to="0" class="active"></li>
-			                    <li data-target="#main-slide" data-slide-to="1"></li>
-			                </ol>
-			                <!--/ Indicators end-->
+				                <!-- Indicators -->
+				                <ol class="carousel-indicators">
+				                    <li data-target="#main-slide" data-slide-to="0" class="active"></li>
+				                    <li data-target="#main-slide" data-slide-to="1"></li>
+				                </ol>
+				                <!--/ Indicators end-->
 
-			                <!-- Carousel inner -->
-			                <div class="carousel-inner">
-			                    <div class="item active">
-			                        <img class="img-responsive" src="images/banner1.jpg" alt="slider">
-			                        <div class="slider-content">
-			                            <div class="col-md-12 text-center">
-			                                <h2 class="animated2">
-			                        		  <span>Welcome to <strong>Margo</strong></span>
-			                        	    </h2>
-			                                <h3 class="animated3">
-			                            		<span>The ultimate aim of your business</span>
-			                            	</h3>
-			                                <p class="animated4"><a href="#" class="slider btn btn-system btn-large">Check Now</a>
-			                                </p>
-			                            </div>
-			                        </div>
-			                    </div>
-			                    <!--/ Carousel item end -->
-			                    <div class="item">
-			                        <img class="img-responsive" src="images/banner1.jpg" alt="slider">
-			                        <div class="slider-content">
-			                            <div class="col-md-12 text-center">
-			                                <h2 class="animated4">
-			                                <span><strong>Margo</strong> for the highest</span>
-			                            </h2>
-			                                <h3 class="animated5">
-			                            	<span>The Key of your Success</span>
-			                            </h3>	
-			                                <p class="animated6"><a href="#" class="slider btn btn-system btn-large">Buy Now</a>
-			                                </p>
-			                            </div>
-			                        </div>
-			                    </div>
-			                    <!--/ Carousel item end -->
-			                </div>
-			                <!-- Carousel inner end-->
+				                <!-- Carousel inner -->
+				                <div class="carousel-inner">
+				                    <div class="item active">
+				                        <img class="img-responsive" src="images/banner1.jpg" alt="slider">
+				                        <div class="slider-content">
+				                            <div class="col-md-12 text-center">
+				                                <!-- <h2 class="animated2">
+				                        		  <span>Welcome to <strong>Margo</strong></span>
+				                        	    </h2>
+				                                <h3 class="animated3">
+				                            		<span>The ultimate aim of your business</span>
+				                            	</h3>
+				                                <p class="animated4"><a href="#" class="slider btn btn-system btn-large">Check Now</a>
+				                                </p> -->
+				                            </div>
+				                        </div>
+				                    </div>
+				                    <!--/ Carousel item end -->
+				                    <div class="item">
+				                        <img class="img-responsive" src="images/banner1.jpg" alt="slider">
+				                        <div class="slider-content">
+				                            <!-- <div class="col-md-12 text-center">
+				                                <h2 class="animated4">
+				                                <span><strong>Margo</strong> for the highest</span>
+				                            </h2>
+				                                <h3 class="animated5">
+				                            	<span>The Key of your Success</span>
+				                            </h3>	
+				                                <p class="animated6"><a href="#" class="slider btn btn-system btn-large">Buy Now</a>
+				                                </p>
+				                            </div> -->
+				                        </div>
+				                    </div>
+				                    <!--/ Carousel item end -->
+				                </div>
+				                <!-- Carousel inner end-->
 
-			                <!-- Controls -->
-			                <a class="left carousel-control" href="#main-slide" data-slide="prev">
-			                    <span><i class="fa fa-angle-left"></i></span>
-			                </a>
-			                <a class="right carousel-control" href="#main-slide" data-slide="next">
-			                    <span><i class="fa fa-angle-right"></i></span>
-			                </a>
-			            </div>
-			            <!-- /carousel -->
-			        </section>
-        		<!-- End Home Page Slider -->
+				                <!-- Controls -->
+				                <a class="left carousel-control" href="#main-slide" data-slide="prev">
+				                    <span><i class="fa fa-angle-left"></i></span>
+				                </a>
+				                <a class="right carousel-control" href="#main-slide" data-slide="next">
+				                    <span><i class="fa fa-angle-right"></i></span>
+				                </a>
+				            </div>
+				            <!-- /carousel -->
+				        </section>
+	        		<!-- End Home Page Slider -->
+				</div>
+				<!-- End Service Icon 1 -->
+				
+				<!-- Start Service Icon 2 -->
+				<div class="col-md-3 hidden-xs" style="text-align:center">
+					<div style="max-height:156px; overflow:hidden; text-align:center" align="center">
+						<img src="images/banner_home01.jpg">
+					</div>
+					<div style="max-height:156px; overflow:hidden; text-align:center">
+						<img src="images/glasses5.jpg">
+					</div>
+					<div style="max-height:158px; overflow:hidden; text-align:center">
+						<img src="images/glasses6.jpg">
+					</div>
+				</div>
+				<!-- End Service Icon 2 -->
+
+				<div class="col-md-3 hidden-sm hidden-md hidden-lg">
+					<div style="padding:0 10px">
+						<div class="projects-carousel touch-carousel">
+							
+							<div class="portfolio-item item">
+								<div class="portfolio-border">
+									<img src="images/banner_home01.jpg">
+								</div>
+							</div>
+							<div class="portfolio-item item">
+								<div class="portfolio-border">
+									<img src="images/glasses5.jpg">
+								</div>
+							</div>
+							<div class="portfolio-item item">
+								<div class="portfolio-border">
+									<img src="images/glasses6.jpg">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
 			</div>
-			<!-- End Service Icon 1 -->
-			
-			<!-- Start Service Icon 2 -->
-			<div class="col-md-3">
-				<div style="max-height:156px; overflow:hidden">
-					<img src="images/banner_home01.jpg">
-				</div>
-				<div style="max-height:156px; overflow:hidden">
-					<img src="images/glasses5.jpg">
-				</div>
-				<div style="max-height:158px; overflow:hidden">
-					<img src="images/glasses6.jpg">
-				</div>
-			</div>
-			<!-- End Service Icon 2 -->
-			
+			<!-- End Services Icons -->
 		</div>
-		<!-- End Services Icons -->
+	</div>
+	<div class="container">
 		<!-- divider -->
 		<div class="hr1 margin-top"></div>
 
@@ -378,6 +405,92 @@
 			</div>
 		</div>
 		<!-- End Clients Carousel -->
+
+		<div class="hr1 margin-top"></div>
+
+
+		<div class="row">
+			<div class="col-md-12">
+				<div style="border:5px solid #EEE; padding:20px 0; text-align:center; font-weight:400; font-size:30px; background:url(images/patterns/6.png);">
+					Download e-Flyer
+				</div>
+			</div>
+		</div>
+		
+		<div class="hr1 margin-top"></div>
+
+		<div class="row">
+
+			<h4 class="classic-title"><span>SPAR Services</span></h4>
+
+            <!-- Start Service Icon 1 -->
+            <div class="col-md-4 col-sm-4 service-box service-icon-left-more">
+                <div class="service-icon">
+                    <i class="fa fa-globe fa-4x"></i>
+                </div>
+                <div class="service-content">
+                    <h4>Home Delivery</h4>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                </div>
+            </div>
+            <!-- End Service Icon 1 -->
+
+              <!-- Start Service Icon 2 -->
+              <div class="col-md-4 col-sm-4 service-box service-icon-left-more">
+               <div class="service-icon">
+                 <i class="fa fa-magic fa-4x"></i>
+               </div>
+               <div class="service-content">
+                <h4>Cash on Delivery</h4>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+              </div>
+            </div>
+            <!-- End Service Icon 2 -->
+
+            <!-- Start Service Icon 3 -->
+            <div class="col-md-4 col-sm-4 service-box service-icon-left-more">
+                <div class="service-icon">
+                    <i class="fa fa-rocket fa-4x"></i>
+                </div>
+                <div class="service-content">
+                <h4>Made to Order Furniture</h4>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                </div>
+            </div>
+            <!-- End Service Icon 3 -->
+             <!-- Start Service Icon 1 -->
+            <div class="col-md-2 col-sm-2 service-box service-icon-left-more">
+            </div>
+            <div class="col-md-4 col-sm-4 service-box service-icon-left-more">
+                <div class="service-icon">
+                    <i class="fa fa-globe fa-4x"></i>
+                </div>
+                <div class="service-content">
+                    <h4>Made to Order Cake</h4>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                </div>
+            </div>
+            <!-- End Service Icon 1 -->
+
+              <!-- Start Service Icon 2 -->
+              <div class="col-md-4 col-sm-4 service-box service-icon-left-more">
+               <div class="service-icon">
+                 <i class="fa fa-magic fa-4x"></i>
+               </div>
+               <div class="service-content">
+                <h4>Private Catering</h4>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+              </div>
+            </div>
+            <!-- End Service Icon 2 -->
+            <div class="col-md-2 col-sm-2 service-box service-icon-left-more">
+            </div>
+
+        </div>
+
+
+		<div class="hr1 margin-top"></div>
+
 		
 	</div>
 </div>
